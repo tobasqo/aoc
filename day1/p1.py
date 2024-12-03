4b992b14c783e8309233976c10da4749c803c83d
tobasqo <szymon.j.jaskowski@gmail.com>
2024-12-03 13:53:41 +0100

    day1

---
+++ day1/p1.py
@@ -0,0 +1,32 @@
+import heapq
+
+heappop = heapq.heappop
+
+INPUT = 'day1/.input'
+# INPUT = 'day1/example.input'
+
+
+def load_input():
+    ls = []
+    rs = []
+    with open(INPUT) as f:
+        for line in f.read().splitlines():
+            l, r = map(int, line.split())
+            ls.append(l)
+            rs.append(r)
+    return ls, rs
+
+
+def main():
+    res = 0
+    ls, rs = load_input()
+    heapq.heapify(ls)
+    heapq.heapify(rs)
+    while ls and rs:
+        l = heappop(ls)
+        r = heappop(rs)
+        res += abs(l - r)
+    print(res)
+
+if __name__ == "__main__":
+    main()
---
+++ day1/p2.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+INPUT = 'day1/.input'
+# INPUT = 'day1/example.input'
+
+
+def load_input():
+    ls = []
+    rs = []
+    with open(INPUT) as f:
+        for line in f.read().splitlines():
+            l, r = map(int, line.split())
+            ls.append(l)
+            rs.append(r)
+    return ls, rs
+
+
+def main():
+    res = 0
+    ls, rs = load_input()
+    rs_cnt = Counter(rs)
+    for l in ls:
+        res += l * rs_cnt.get(l, 0)
+    print(res)
+
+if __name__ == "__main__":
+    main()
