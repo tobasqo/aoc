4d23f2f32c76da3f13171c07af04e1989affd2e0
tobasqo <szymon.j.jaskowski@gmail.com>
2024-12-22 16:19:37 +0100

    d4p1 example working

---
+++ day4/p1.py
@@ -0,0 +1,46 @@
+# INPUT = "day4/.input"
+INPUT = "day4/example.input"
+
+
+def load_input():
+    with open(INPUT) as f:
+        return f.read().splitlines()
+
+
+def gen_directions():
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if dx == 0 and dy == 0:
+                continue
+            yield dx, dy
+
+
+def in_bounds(x: int, max_x: int, y: int, max_y: int):
+    return 0 <= x < max_x and 0 <= y < max_y
+
+
+def main():
+    res = 0
+    input = load_input()
+    m = len(input)
+    n = len(input[0])
+    for row in range(m):
+        for col in range(n):
+            for dx, dy in gen_directions():
+                found = True
+                for i, c in enumerate("XMAS"):
+                    row_delta = row + i * dx
+                    col_delta = col + i * dy
+                    if not in_bounds(row_delta, m, col_delta, n):
+                        found = False
+                        break
+                    if input[row_delta][col_delta] != c:
+                        found = False
+                        break
+                if found:
+                    res += 1
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
