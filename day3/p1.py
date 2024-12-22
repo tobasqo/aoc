2b71dc0348c2a8c9b4e72c788bdb4ba675e2bed1
tobasqo <szymon.j.jaskowski@gmail.com>
2024-12-22 12:55:48 +0100

    d3p1 working example

---
+++ day3/p1.py
@@ -0,0 +1,24 @@
+import re
+
+INPUT = 'day3/.input'
+# INPUT = 'day3/example.input'
+
+MUL_PATTERN = re.compile(r'mul\(\d{1,3},\d{1,3}\)')
+
+
+def load_input():
+    with open(INPUT) as f:
+        return f.read()
+
+
+def main():
+    res = 0
+    input = load_input()
+    for match in re.findall(MUL_PATTERN, input):
+        a, b = map(int, match[4:-1].split(','))
+        res += a * b
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
